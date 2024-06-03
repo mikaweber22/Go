2c9b6c592601fad530914f84f72c0f289878907e
paulutsch <unoutsch@gmail.com>
2024-06-03 18:30:54 +0200

    feat: neue Hausgaben Datenstrukturen

--- 02 Datenstrukturen/hausaufgabe.md
+++ 02 Datenstrukturen/hausaufgabe.md
@@ -7,3 +7,11 @@
 1. Schaue dir das beispiel.ipynb an und gehe noch mal alles durch.
 2. Überlege dir für jede Datenstruktur ein weiteres sinnvolles Beispiel und erstelle eine entsprechende Variable.
 3. BONUS: Füge zu jeder Datenstruktur, die man verändern darf, ein Element hinzu und entferne ein Element. Greife von jeder Datenstruktur auf ein Element zu.
+
+---
+
+1. Schaue dir die Kollektivaufgabe an und überlege dir, wie du die Aufgabe lösen würdest.
+2. Löse die Aufgaben 1, 2 und 3. Stelle dir bei 2 und 3 jeweils folgende Fragen:
+
+-   Ist die Datenstruktur, die ich mir ausgesucht habe, geordnet? Ist das gut so? Wenn nein, welche andere Datenstruktur könnte ich verwenden?
+-   Kann ich die Datenstruktur verändern? Ist das gut so? Wenn nein, welche andere Datenstruktur könnte ich verwenden?
--- 02 Datenstrukturen/kollektivaufgabe.py
+++ 02 Datenstrukturen/kollektivaufgabe.py
@@ -1,4 +1,39 @@
-# Ziel: Erfolgseinträge als Wörterbuch-Struktur erstellen
+# Erstelle die sinnvollste Datenstruktur aus den Datenstruktur-Typen, die wir kennengelernt haben, für folgendes Problem:
+# Wir wollen alle Kursteilnehmer auf eine Weise einspeichern, die uns all die Möglichkeiten gibt, die wir brauchen.
 
-# erfolge = ...?
+# Wir wollen folgende Eigenschaften pro Kursteilnehmer speichern:
+# - Teilnehmer-ID
+# - Vorname
+# - Alter
+# - Lieblingsessen
+# - Vornamen der drei besten Freunde
 
+
+"""Aufgaben:
+1. Was muss unsere Datenstruktur können?
+- wir müssen die Informationen der Kursteilehmer ändern können
+- wir müssen neue Teilnehmer hinzufügen können
+2. In welcher Datenstruktur können wir einzelne Kursteilnehmer einspeichern?
+3. In welcher Datenstruktur können wir alle Kursteilnehmer einspeichern?
+"""
+
+# --- Mika's Idee ---
+Kursteimehmer_info = {
+    "1": ["Mika", 14, "Burger", "Duncan, Ralf, Vincent"],
+    "2": ["Lucy", 10, "Nudeln", "Sarah, Mira, Sabina"],
+    "3": ["Hanno", 12, "Nudeln", "Anton, Philip, Ben"],
+}
+
+# --- Lucy's Idee ---
+lucy = [1, 10, "Nudeln", "Sarah, Mira, Sabina"]
+mika = [2, 14, "Burger", "Duncan, Ralf, Vincent"]
+hanno = [3, 12, "Nudeln", "Anton, Philip, Ben"]
+kursteilnehmer = [lucy, mika, hanno]
+# ist das gleiche wie oben
+kursteilnehmer = [
+    [1, "lucy", 10, "Nudeln", "Sarah, Mira, Sabina"],
+    [2, "mika", 14, "Burger", "Duncan, Ralf, Vincent"],
+    [3, "hanno", 12, "Nudeln", "Anton, Philip, Ben"],
+]
+
+# --- Hanno's Idee ---
