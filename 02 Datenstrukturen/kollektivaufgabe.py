78f0f8395dc574889f238282a3ba75a33a263ce1
paulutsch <unoutsch@gmail.com>
2024-06-10 18:27:38 +0200

    feat: Datenstrukturem

--- 02 Datenstrukturen/kollektivaufgabe.py
+++ 02 Datenstrukturen/kollektivaufgabe.py
@@ -17,23 +17,76 @@
 3. In welcher Datenstruktur können wir alle Kursteilnehmer einspeichern?
 """
 
-# --- Mika's Idee ---
-Kursteimehmer_info = {
+kursteilnehmer_paul = [
+    {
+        "id": 1,
+        "name": "Lucy",
+        "alter": 10,
+        "lieblingsessen": "Nudeln",
+        "freunde": ["Sarah", "Mira", "Sabina"],
+    },
+    {
+        "id": 2,
+        "name": "Mika",
+        "alter": 14,
+        "lieblingsessen": "Burger",
+        "freunde": ["Duncan", "Ralf", "Vincent"],
+    },
+    {
+        "id": 3,
+        "name": "Hanno",
+        "alter": 12,
+        "lieblingsessen": "Nudeln",
+        "freunde": ["Anton", "Philip", "Ben"],
+    },
+]
+
+kursteilnehmer_mika = {
     "1": ["Mika", 14, "Burger", "Duncan, Ralf, Vincent"],
     "2": ["Lucy", 10, "Nudeln", "Sarah, Mira, Sabina"],
     "3": ["Hanno", 12, "Nudeln", "Anton, Philip, Ben"],
 }
 
-# --- Lucy's Idee ---
-lucy = [1, 10, "Nudeln", "Sarah, Mira, Sabina"]
-mika = [2, 14, "Burger", "Duncan, Ralf, Vincent"]
-hanno = [3, 12, "Nudeln", "Anton, Philip, Ben"]
-kursteilnehmer = [lucy, mika, hanno]
-# ist das gleiche wie oben
-kursteilnehmer = [
+kursteilnehmer_lucy = [
     [1, "lucy", 10, "Nudeln", "Sarah, Mira, Sabina"],
     [2, "mika", 14, "Burger", "Duncan, Ralf, Vincent"],
     [3, "hanno", 12, "Nudeln", "Anton, Philip, Ben"],
 ]
 
-# --- Hanno's Idee ---
+""""
+Kollektivaufgabe:
+1. Erstelle einen for-Loop für Mika's Idee, der den Namen aller Kursteilnehmer ausgibt.
+2. Erstelle einen for-Loop für Paul's Idee, der das Lieblingsessen aller Kursteilnehmer ausgibt.
+3. Erstelle einen for-Loop für Lucy's Idee, der das Alter aller Kursteilnehmer ausgibt.
+"""
+
+# Beispiel für for-Loop
+# zahlen = [1, 2, 3, 4, 5]
+# for sdghfgj in zahlen:
+#     print(sdghfgj)
+
+# Hanno: Lucy's Idee
+print("\n Hanno's Idee")
+kursteilnehmer_lucy = [
+    [1, "lucy", 10, "Nudeln", "Sarah, Mira, Sabina"],
+    [2, "mika", 14, "Burger", "Duncan, Ralf, Vincent"],
+    [3, "hanno", 12, "Nudeln", "Anton, Philip, Ben"],
+]
+for teilnehmer in kursteilnehmer_lucy:
+    print(teilnehmer)
+
+
+# Mika: Mika's Idee
+# print("\n Mika's Idee")
+# for i in kursteilnehmer_mika:
+#     if kursteilnehmer_mika["1"] or kursteilnehmer_mika["2"] or kursteilnehmer_mika["3"]:
+#         print(kursteilnehmer_mika["1"]["2"]["3"][1])
+
+
+# Lucy: Paul's Idee
+print("\n Lucy's Idee")
+for kursteilnehmer in kursteilnehmer_paul:
+    # kursteilnehmer ist ein Dictionary
+    # Deshalb rufen wir die Werte mit den Schlüsseln ab
+    lieblingsessen = kursteilnehmer["lieblingsessen"]
+    print(lieblingsessen)
