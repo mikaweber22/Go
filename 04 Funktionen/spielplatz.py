7079e5925f8f25455ce7f5a59ccf682bca652438
paulutsch <unoutsch@gmail.com>
2024-08-12 18:02:13 +0200

    feat: Funktionen
    Co-authored-by: lucymatic <lucymatic@users.noreply.github.com>
    Co-authored-by: mikaweber22 <mikaweber22@users.noreply.github.com>

---
+++ 04 Funktionen/spielplatz.py
@@ -0,0 +1,31 @@
+import random
+
+import daten
+
+
+def satzbausteine():
+    subjekt = random.choice(daten.subjekte)
+    prädikat_und_objekt = random.choice(daten.prädikate_und_objekte)
+    adverbialbestimmungen = random.choice(daten.adverbialbestimmungen)
+    abverb = random.choice(daten.adverbien)
+    twist = random.choice(daten.twists)
+    return subjekt, prädikat_und_objekt, adverbialbestimmungen, abverb, twist
+
+
+for i in range(5):
+    satzbausteine()
+
+    satz = (
+        subjekt
+        + " "
+        + prädikat_und_objekt
+        + " "
+        + adverbialbestimmungen
+        + " "
+        + abverb
+        + ", "
+        + twist
+        + "."
+    )
+
+    print(satz + "\n")
