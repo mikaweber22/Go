811f135399c6e89eec94fb05da5e54ad119e447d
paulutsch <unoutsch@gmail.com>
2024-09-02 18:14:52 +0200

    feat: Hausaufgaben Klassen
    Co-authored-by: Lucymatic <lucymatic@users.noreply.github.com>
    Co-authored-by: mikaweber22 <mikaweber22@users.noreply.github.com>

--- 05 Klassen/kollektivaufgabe.py
+++ 05 Klassen/kollektivaufgabe.py
@@ -16,9 +16,26 @@ class Auto:
     def __init__(self, marke, farbe, maximale_geschwindigkeit, beschleunigung, baujahr):
         self.marke = marke
         self.farbe = farbe
+        self.geschwindigkeit = 0
         self.maximale_geschwindigkeit = maximale_geschwindigkeit
         self.beschleunigung = beschleunigung
         self.baujahr = baujahr
+
+    def beschleunigen(self):
+        if self.geschwindigkeit > self.maximale_geschwindigkeit - 10:
+            self.geschwindigkeit = self.maximale_geschwindigkeit
+            print("das Auto fährt so schnell wie möglich")
+            return
+        self.geschwindigkeit = self.geschwindigkeit + 10
+        print(f"Das Auto fährt mit {self.geschwindigkeit} km/h.")
+
+    def bremsen(self):
+        if self.geschwindigkeit < 10:
+            self.geschwindigkeit = 0
+            print("Das Auto steht.")
+            return
+        self.geschwindigkeit = self.geschwindigkeit - 10
+        print(f"Das Auto fährt mit {self.geschwindigkeit} km/h.")
 
 
 auto1 = Auto("Ford", "Silber", 290, 4.4, 1970)
